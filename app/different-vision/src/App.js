cb2d01182eeffd47753a555cc1b8dd3504abbcac
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-06-08 11:34:30 +0200

    Firebase werkt, alle kunstwerken staan nu online in database

--- app/different-vision/src/App.js
+++ app/different-vision/src/App.js
@@ -1,15 +1,48 @@
 import React, { Component } from "react";
+import firebase from "firebase/app";
+import Rebase from "re-base";
+import database from "firebase/database";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { kunstwerken: [] };
+
+    // Firebase
+    const config = {
+      apiKey: "AIzaSyBqKXTejPfFjJH5_yaxapBijSnQo2VSMuk",
+      authDomain: "rotterdam-maiv.firebaseapp.com",
+      databaseURL: "https://rotterdam-maiv.firebaseio.com"
+    };
+    const app = firebase.initializeApp(config);
+    const db = firebase.database(app);
+    this.base = Rebase.createClass(db);
+  }
+
+  componentDidMount = () => {
+    this.base.syncState(`kunstwerken`, {
+      context: this,
+      state: "kunstwerken"
+    });
+  };
+
   render() {
+    const { kunstwerken } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {kunstwerken.map(kunstwerk => {
+          return (
+            <div key={kunstwerk.id} className="kunstwerk">
+              <p>{kunstwerk.title}</p>
+              <p>{kunstwerk.author}</p>
+              <p>{kunstwerk.desc}</p>
+              <img className="kunstwerk-img" src={kunstwerk.img} />
+            </div>
+          );
+        })}
       </div>
     );
   }
