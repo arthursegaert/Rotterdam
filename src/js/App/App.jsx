71d87bfeb12ce5105da43c9a7a5a7ff98a8a51ff
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-06-10 20:26:59 +0200

    Css is in different file now for nav

--- src/js/App/App.jsx
+++ src/js/App/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { app } from "./config/fire.js";
-import { Route, Link, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Logout from "./pages/Logout.jsx";
