e48ad0923786cd057cc77de7e01c818c698bd633
justineVanderplancke <justine.vanderplancke@student.howest.be>
2018-06-11 19:14:19 +0200

    Background werken

--- src/js/App/pages/Werken.jsx
+++ src/js/App/pages/Werken.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { base } from "../config/fire.js";
 import { Link } from "react-router-dom";
 import Nav from "../components/Nav.jsx";
+import "../css/Werken.css";
 
 class Werken extends Component {
   constructor(props) {
@@ -19,31 +20,30 @@ class Werken extends Component {
   }
   render() {
     return (
-      <div>
+      <div className="werken-div-background">
         <Nav />
+        <div className="werken-grid">
         {this.state.kunstwerken.map(kunstwerk => {
           return (
-            <div key={kunstwerk.id} className="kunstwerk">
-              <p>{kunstwerk.title}</p>
-              <p>{kunstwerk.author}</p>
-              <img
-                alt={kunstwerk.desc}
-                className="kunstwerk-img"
-                src={kunstwerk.img}
-                width="1000"
-                height="1000"
-              />
+            <div key={kunstwerk.id} className= {["werken-kunstwerk",["werken-kunstwerk", kunstwerk.id].join('-')].join(' ')}>
+              <p className= {["kunstwerk-author-p",["kunstwerk-author-p", kunstwerk.id].join('-')].join(' ')}>{kunstwerk.author.split(" ").pop()}</p>
               <Link
                 to={{
                   pathname: `/werkdetail/${kunstwerk.id}`,
                   kunstwerk
                 }}
               >
-                Werk Detail
+                <img
+                alt={kunstwerk.desc}
+                className= {["kunstwerk-img", kunstwerk.id].join('-')}
+                src={kunstwerk.img}
+
+              />
               </Link>
             </div>
           );
         })}
+      </div>
       </div>
     );
   }
