889af8effa6db5b900f70240c97d5e342aa5ad66
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-06-12 09:39:55 +0200

    deleted button

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,8 @@
-import anime from "animejs";
 {
-  const handleClickButton = e => {
-    console.log(e);
-    var el = document.querySelector(".box");
-    el.classList.remove("hide");
-
-    const finishedPromise = anime({
-      targets: el,
-      translateX: "100vw",
-      duration: 1000,
-      loop: false,
-      easing: "easeOutQuad"
-    });
-
-    const logFinished = () => {
-      el.removeAttribute("style");
-      el.classList.add("hide");
-    };
-
-    finishedPromise.finished.then(logFinished);
-  };
 
   const init = () => {
     document
-      .querySelector(".button")
-      .addEventListener("click", handleClickButton);
+      .querySelector(".javascript-error")
   };
 
   init();
