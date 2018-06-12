be12887296d06d41984add6df2edd04d41d1a497
Arthur <arthur.segaert@student.howest.be>
2018-06-12 11:07:33 +0200

    Merge fixed

--- src/js/App/pages/Account.jsx
+++ src/js/App/pages/Account.jsx
@@ -80,7 +80,6 @@ class Account extends Component {
                 {this.state.captions.map(caption => (
                   <article className="caption">
                     <h3 className="caption-content">{caption.caption}</h3>
-<<<<<<< HEAD
                     <p className="caption-author">{caption.userName}</p>
                     <a className="caption-link-sticker" href="#b">
                       maak de sticker
@@ -88,7 +87,6 @@ class Account extends Component {
                     <a className="caption-link-share" href="#b">
                       deel
                     </a>
->>>>>>> db3dd8b304a2aee7b490c3619fe16923cd20ad94
                   </article>
                 ))}
                 </div>
