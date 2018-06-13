eae5295cc4479796909067eb5db1c1a3e926351a
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-06-13 20:58:31 +0200

    blue background gradient

--- src/js/App/pages/Account.jsx
+++ src/js/App/pages/Account.jsx
@@ -32,83 +32,87 @@ class Account extends Component {
       <StatusContext.Consumer>
         {({ authenticated }) =>
           authenticated ? (
-            <main className="two-sec-page-account">
-              <h1 className="page-title page-title-account">Mijn Account</h1>
-              <Nav classname="account-nav" />
-              <section className="section-left section-left-account">
-                <h2 className="page-subtitle section-left-subtitle">
-                  Mijn informatie
-                </h2>
-                <form className="section-left-form-account">
-                  <label className="section-left-label">E-mail</label>
-                  <input
-                    required
-                    name="email"
-                    type="email"
-                    className="section-left-input"
-                  />
-                  <label className="section-left-label">Gebruikersnaam</label>
-                  <input
-                    required
-                    name="username"
-                    type="text"
-                    className="section-left-input"
-                  />
-                  <label className="section-left-label">Wachtwoord</label>
-                  <input
-                    name="password"
-                    type="password"
-                    className="section-left-input"
-                  />
-                  <input
-                    className="section-left-form-submit"
-                    type="submit"
-                    value="Save changes"
-                  />
-                </form>
-              </section>
-              <section className="section-right section-right-account">
-                <h2 className="page-subtitle section-right-subtitle">
-                  Mijn captions
-                </h2>
-                <p className="section-right-account-numberupvotes">
-                  58 upvotes
-                </p>
-                <p className="section-right-account-numbercaptions">
-                  {this.state.captions.length} captions
-                </p>
-                <div className="section-right-account-captions">
-                  {this.state.captions.map(caption => (
-                    <Caption
-                      caption={caption}
-                      userName={caption.userName}
-                      deleteOption="true"
+            <div className="account-background">
+              <main className="two-sec-page-account">
+                <h1 className="page-title page-title-account">Mijn Account</h1>
+                <Nav classname="account-nav" />
+                <section className="section-left section-left-account">
+                  <h2 className="page-subtitle section-left-subtitle">
+                    Mijn informatie
+                  </h2>
+                  <form className="section-left-form-account">
+                    <label className="section-left-label">E-mail</label>
+                    <input
+                      required
+                      name="email"
+                      type="email"
+                      className="section-left-input"
                     />
-                  ))}
-                </div>
-              </section>
-            </main>
+                    <label className="section-left-label">Gebruikersnaam</label>
+                    <input
+                      required
+                      name="username"
+                      type="text"
+                      className="section-left-input"
+                    />
+                    <label className="section-left-label">Wachtwoord</label>
+                    <input
+                      name="password"
+                      type="password"
+                      className="section-left-input"
+                    />
+                    <input
+                      className="section-left-form-submit"
+                      type="submit"
+                      value="Save changes"
+                    />
+                  </form>
+                </section>
+                <section className="section-right section-right-account">
+                  <h2 className="page-subtitle section-right-subtitle">
+                    Mijn captions
+                  </h2>
+                  <p className="section-right-account-numberupvotes">
+                    58 upvotes
+                  </p>
+                  <p className="section-right-account-numbercaptions">
+                    {this.state.captions.length} captions
+                  </p>
+                  <div className="section-right-account-captions">
+                    {this.state.captions.map(caption => (
+                      <Caption
+                        caption={caption}
+                        userName={caption.userName}
+                        deleteOption="true"
+                      />
+                    ))}
+                  </div>
+                </section>
+              </main>
+            </div>
           ) : (
-            <main className="two-sec-page-account">
-              <h1 className="page-title">Mijn Account</h1>
-              <Nav />
-              <section className="section-left section-left-account section-left-account-not-signed-in">
-                <Link
-                  className="page-subtitle section-left-subtitle"
-                  to="/login"
-                >
-                  Login
-                </Link>
-              </section>
-              <section className="section-right section-right-account section-right-account-not-signed-in">
-                <Link
-                  className="page-subtitle section-left-subtitle"
-                  to="/register"
-                >
-                  Registreer
-                </Link>
-              </section>
-            </main>
+            <div className="account-background">
+              <main className="two-sec-page-account">
+                <h1 className="page-title">Mijn Account</h1>
+                <Nav />
+                <section className="section-left section-left-account section-left-account-not-signed-in">
+                  <Link
+                    className="page-subtitle section-left-subtitle"
+                    to="/login"
+                  >
+                    Login
+                  </Link>
+                </section>
+                <section className="section-right section-right-account section-right-account-not-signed-in">
+                  <Link
+                    className="page-subtitle section-left-subtitle"
+                    to="/register"
+                  >
+                    Registreer
+                  </Link>
+                </section>
+              </main>
+            </div>
           )
         }
       </StatusContext.Consumer>
