4d30e5eb0119f12cf60f66a9a8f9c1e62c7c7051
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-06-14 21:21:50 +0200

    go back arrow detail zoom page

--- src/js/App/components/imageZoom.jsx
+++ src/js/App/components/imageZoom.jsx
@@ -14,7 +14,19 @@ const imageZoom = ({ kunstwerk, onChange }) => {
         alt={kunstwerk.title}
         src={["../", kunstwerk.img].join("")}
       />
-      <button onClick={goBack}>Go back</button>
+      <svg
+        className="back-arrow-svg"
+        onClick={goBack}
+        viewBox="0 0 512 512"
+        width="100"
+      >
+        <path
+          className="back-arrow"
+          d="M167.2,430.6c7,7.2,18.6,7.2,25.8,0c7-7,7-18.6,0-25.5L62.2,274.2h431.7c10.1,0,18.1-8,18.1-18.1
+	c0-10.1-8-18.3-18.1-18.3H62.2L193,107.2c7-7.2,7-18.8,0-25.8c-7.2-7.2-18.8-7.2-25.8,0L5.4,243.2c-7.2,7-7.2,18.6,0,25.5
+	L167.2,430.6z"
+        />
+      </svg>
     </div>
   );
 };
