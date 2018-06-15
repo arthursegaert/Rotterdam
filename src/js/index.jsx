a4ae50397ae9ca6dd2bb6978fb2cd10664bfd724
Arthur <arthur.segaert@student.howest.be>
2018-06-15 08:56:45 +0200

    Revert "favicon added"
    This reverts commit a3671d567fe7ce8fa19dd8ae52dc1ff3e5acd3d6.

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App/App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./App/components/ScrollToTop.jsx";
 import WebfontLoader from "@dr-kobros/react-webfont-loader";
 
@@ -13,19 +13,24 @@ import archivo from "../assets/fonts/archivo/archivo.css";
 const config = {
   custom: {
     families: ["circular", "sinkinsans", "archivo", "integral"],
-    urls: [circular, sinkinsans, archivo, integral]
+    urls: [
+      circular,
+      sinkinsans,
+      archivo,
+      integral
+    ]
   }
 };
 
 /* basename */
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <ScrollToTop>
       <WebfontLoader config={config}>
         <App />
       </WebfontLoader>
     </ScrollToTop>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById(`reactcontent`)
 );
